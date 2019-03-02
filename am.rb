4005d9a972c21b64c6a030d7dd1661436e3c86ac
Miguel Torres <migueltg93@gmail.com>
2019-03-02 13:07:25 -0600

    Initial commit! 🚀

---
+++ am.rb
@@ -0,0 +1,34 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class AM
+  attr_reader :title, :author, :date, :content
+
+  def initialize(url)
+    @doc = Nokogiri::HTML(open(url))
+    @title = get_title
+    @author = get_author
+    @date = get_date
+    @content = get_content
+  end
+
+  private
+
+    def get_title
+      @doc.xpath('/html/body/main/article/div/header/h1').children.text
+    end
+
+    def get_author
+      @doc.xpath('/html/body/main/article/div/header/div[2]/p/span').text.strip
+    end
+
+    def get_date
+      @doc.xpath('/html/body/main/article/div/header/div[2]/div[2]/time').text
+    end
+
+    def get_content
+      @doc.css('/html/body/main/article/div/div/div[2]/div[2]/p').text
+    end
+end
+
---
+++ am_spec.rb
@@ -0,0 +1,36 @@
+require 'rspec'
+require_relative './am.rb'
+
+RSpec.describe AM, "stuff" do
+  let(:url) { 'https://www.am.com.mx/noticias/Arriba-Guaido-a-Buenos-Aires-sigue-desafiando-a-Maduro-20190302-0008.html' }
+  let(:article) { AM.new url }
+
+  describe "#new" do
+    it "parses the article's title correctly" do
+      expect(article.title).to match(/Arriba Guaidó a Buenos Aires/)
+    end
+
+    it "parses the article's date correctly" do
+      expect(article.date).to match(/02 de Marzo/)
+    end
+
+    it "parses the article's author correctly" do
+      expect(article.author).to match(/Redacción AM/)
+    end
+
+    it "parses the article's tags correctly" do
+      skip
+      expect(article.date).to match(/02 de Marzo/)
+    end
+     
+    it "parses the article's content correctly" do
+      expect(article.content).to match(/El titular de la Asamblea Nacional/)
+    end
+
+    it "parses the article's images correctly" do
+      skip
+      expect(article.date).to match(/02 de Marzo/)
+    end
+
+  end
+end
