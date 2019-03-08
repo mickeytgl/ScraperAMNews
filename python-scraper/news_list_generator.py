4f6ae35979fa609435284d44a4a5ffd8d65dc13b
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2019-03-07 20:42:40 -0600

    Uploaded Python base scraper.

---
+++ python-scraper/news_list_generator.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from time import sleep
+import requests
+import argparse
+
+# Download and install Chromedriver:
+# https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+# Global:
+pagination = 10
+out_file = 'links_to_news.txt'
+chromedriver_path = './chromedriver'  # For downloaded version
+
+# Chromedriver settings:
+verbose = True
+options = webdriver.ChromeOptions()
+if not verbose:
+    options.add_argument('headless')
+
+# Construct the argument parser:
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--pages", type=int, default=pagination,
+                help="Number of pages to scrape.")
+ap.add_argument("-f", "--file", default=out_file,
+                help="File to save the news list.")
+args = vars(ap.parse_args())
+
+
+def news_list_generator(pagination, out_file):
+    # Browser launch with chromedriver:
+    # browser = webdriver.Chrome(chrome_options=options)
+
+    # Or for downloaded version (from specific path):
+    browser = webdriver.Chrome(executable_path=chromedriver_path,
+                               chrome_options=options)
+
+    # Set url and open it on Chrome:
+    url = 'https://www.am.com.mx/seccion/noticias/'
+    browser.get(url)
+
+    # Search molecule:
+    sleep(2)
+    flag = scrape_news(browser, pagination, out_file)
+
+    # Close browser:
+    browser.quit()
+    return
+
+
+def scrape_news(browser, pagination, out_file_name):
+    # Look for button and click it:
+    button_xpath = '//*[@class="viewmore"]/button'
+    browser.find_element_by_xpath(button_xpath).click()
+    sleep(1)
+
+    # Get links to news in an iterative way according to pagination:
+    with open(out_file, "w+") as fout:
+        displayed_news_per_view = 8
+        initial_article_index = 6
+        for view in range(pagination):
+            view *= displayed_news_per_view
+            for index in range(displayed_news_per_view):
+                xpath = '//*[@class="col__main"]/article[{}]/div/h2/a'
+                xpath = xpath.format(initial_article_index + view + index)
+                news = browser.find_element_by_xpath(xpath)
+                news_url = news.get_attribute('href')
+                fout.write(news_url + '\n')
+
+            # Search for button again:
+            browser.find_element_by_xpath(button_xpath).click()
+            sleep(1)
+
+    return
+
+
+if __name__ == '__main__':
+    pages = args['pages']
+    out_file = args['file']
+    news_list_generator(pages, out_file)
